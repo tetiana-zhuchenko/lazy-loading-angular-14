339ce76dfbc9e6a4a10e404e5105cc11d6266695
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-10 12:48:44 +0200

    fix: fixed lazy loading

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,11 +1,34 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import { TasksComponent } from '../tasks/tasks.component';
 import {
   NewTaskComponent,
   canLeaveEditPage,
 } from '../tasks/new-task/new-task.component';
-import { Type } from '@angular/core';
+import { inject, Type } from '@angular/core';
+import { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
 
 export const routes: Routes = [
   {

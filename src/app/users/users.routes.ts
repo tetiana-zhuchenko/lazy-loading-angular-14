00f99de3b9b0fb2f48cf63166aa9af79b82b373a
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-17 17:19:05 +0200

    feat: added lazy loading for service

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -5,29 +5,36 @@ import {
   NewTaskComponent,
   canLeaveEditPage,
 } from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    // loadComponent: () => {
-    //   return import('../tasks/tasks.component').then(
-    //     (mod) => mod.TasksComponent
-    //   );
-    // },
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => {
+        //   return import('../tasks/tasks.component').then(
+        //     (mod) => mod.TasksComponent
+        //   );
+        // },
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
